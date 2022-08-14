8e4e8cc1339549bb9af5916071c743bbc14a5916
benjaminburton <benjaminlburton@gmail.com>
2022-08-13 22:23:27 -0700

    committing

---
+++ state-of-react/src/Square.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Square extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>VELSMARIAM</h1>
+        );
+    }
+}
+ 
+export default Square;
